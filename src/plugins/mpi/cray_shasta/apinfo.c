46e2b9f1934f035b2973805bbf0f9ab7a1eaa267
David Gloe <dgloe@cray.com>
2019-07-02 11:13:08 -0500

    mpi/cray_shasta: MPMD support
    Add support for MPMD, through either heterogeneous or multi-prog job
    steps. This is done by creating the command array and filling in the
    command index for each rank from Slurm's internal data structures.
    
    For heterogeneous job steps (AKA job packs), the MPMD information is
    available through the pack_* step struct members. For multi-prog job
    steps, the MPMD information is available in the config file stored in
    argv[0].
    
    Tested by running the PALS pmi_example with normal, heterogeneous, and
    multi-prog job steps.

--- src/plugins/mpi/cray_shasta/apinfo.c
+++ src/plugins/mpi/cray_shasta/apinfo.c
@@ -2,7 +2,7 @@
  * apinfo.c - Cray Shasta PMI apinfo file creation
  * Copyright 2019 Cray Inc. All Rights Reserved.
  */
-#include <assert.h>
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
@@ -27,36 +27,202 @@ static int _get_nid(const char *hostname)
 }
 
 /*
- * Return an array of pals_pe_t structures. Returns NULL on error.
+ * Parse an MPMD file to determine the number of MPMD commands and task->cmd
+ * mapping. Adopted from multi_prog_parse in src/slurmd/slurmstepd/multi_prog.c.
+ *
+ * The file's contents are stored in job->argv[1], and follow this format:
+ * <taskids> <command> <arguments>
+ *
+ * taskids is a range list of task IDs or * (for all remaining task IDs).
+ * command and arguments give the argv to run for those tasks.
+ * Empty lines and lines starting with # are ignored.
+ * Newlines may be escaped with \.
  */
-static pals_pe_t *_setup_pals_pes(const stepd_step_rec_t *job)
+void _multi_prog_parse(const stepd_step_rec_t *job, int *ncmds,
+		       uint32_t **tid_offsets)
+{
+	int i = 0, line_num = 0, rank_id = 0, num_cmds = 0, nranks = 0;
+	char *line = NULL, *local_data = NULL;
+	char *end_ptr = NULL, *save_ptr = NULL, *tmp_str = NULL;
+	char *rank_spec = NULL, *p = NULL, *one_rank = NULL;
+	hostlist_t hl;
+	uint32_t *offsets = NULL;
+
+	offsets = xmalloc(job->ntasks * sizeof(uint32_t));
+	for (i = 0; i < job->ntasks; i++) {
+		offsets[i] = NO_VAL;
+	}
+
+	// Copy contents of MPMD file so we can tokenize it
+	local_data = xstrdup(job->argv[1]);
+
+	// Replace escaped newlines with spaces
+	while ((p = xstrstr(local_data, "\\\n")) != NULL) {
+		p[0] = ' ';
+		p[1] = ' ';
+	}
+
+	while (1) {
+		// Get the next line
+		if (line_num)
+			line = strtok_r(NULL, "\n", &save_ptr);
+		else
+			line = strtok_r(local_data, "\n", &save_ptr);
+		if (!line)
+			break;
+		line_num++;
+
+		// Get task IDs from the line
+		p = line;
+		while ((*p != '\0') && isspace(*p)) /* remove leading spaces */
+			p++;
+		if (*p == '#') /* only whole-line comments handled */
+			continue;
+		if (*p == '\0') /* blank line ignored */
+			continue;
+
+		rank_spec = p; /* Rank specification for this line */
+		while ((*p != '\0') && !isspace(*p))
+			p++;
+		if (*p == '\0')
+			goto fail;
+		*p++ = '\0';
+
+		while ((*p != '\0') && isspace(*p)) /* remove leading spaces */
+			p++;
+		if (*p == '\0') /* blank line ignored */
+			continue;
+
+		nranks = 0;
+		// If rank_spec is '*', set all remaining ranks to this cmd
+		if (!xstrcmp(rank_spec, "*")) {
+			for (i = 0; i < job->ntasks; i++) {
+				if (offsets[i] == NO_VAL) {
+					offsets[i] = num_cmds;
+					nranks++;
+				}
+			}
+		} else {
+			// Parse rank list into individual ranks
+			xstrfmtcat(tmp_str, "[%s]", rank_spec);
+			hl = hostlist_create(tmp_str);
+			xfree(tmp_str);
+			if (!hl)
+				goto fail;
+			while ((one_rank = hostlist_pop(hl))) {
+				rank_id = strtol(one_rank, &end_ptr, 10);
+				if ((end_ptr[0] != '\0') || (rank_id < 0) ||
+				    (rank_id >= job->ntasks)) {
+					free(one_rank);
+					hostlist_destroy(hl);
+					error("mpi/cray_shasta: invalid rank "
+					      "id %s",
+					      one_rank);
+					goto fail;
+				}
+				free(one_rank);
+
+				offsets[rank_id] = num_cmds;
+				nranks++;
+			}
+			hostlist_destroy(hl);
+		}
+		// Only count this command if it had at least one rank
+		if (nranks > 0) {
+			num_cmds++;
+		}
+	}
+
+	// Make sure we've initialized all ranks
+	for (i = 0; i < job->ntasks; i++) {
+		if (offsets[i] == NO_VAL) {
+			error("mpi/cray_shasta: no command for task id %d", i);
+			goto fail;
+		}
+	}
+
+	xfree(local_data);
+	*ncmds = num_cmds;
+	*tid_offsets = offsets;
+	return;
+
+fail:
+	xfree(offsets);
+	xfree(local_data);
+	*ncmds = 0;
+	*tid_offsets = NULL;
+	return;
+}
+
+/*
+ * Return an array of pals_pe_t structures.
+ */
+static pals_pe_t *_setup_pals_pes(int ntasks, int nnodes, uint16_t *task_cnts,
+				  uint32_t **tids, uint32_t *tid_offsets)
 {
 	pals_pe_t *pes = NULL;
 	int nodeidx, localidx, taskid;
 
-	assert(job->ntasks > 0);
-
-	pes = xmalloc(job->ntasks * sizeof(pals_pe_t));
-	for (nodeidx = 0; nodeidx < job->nnodes; nodeidx++) {
-		for (localidx = 0;
-		     localidx < job->msg->tasks_to_launch[nodeidx];
-		     localidx++) {
-			taskid = job->msg->global_task_ids[nodeidx][localidx];
-			assert(taskid < job->ntasks);
+	pes = xmalloc(ntasks * sizeof(pals_pe_t));
+	for (nodeidx = 0; nodeidx < nnodes; nodeidx++) {
+		for (localidx = 0; localidx < task_cnts[nodeidx]; localidx++) {
+			taskid = tids[nodeidx][localidx];
+			if (taskid >= ntasks) {
+				error("mpi/cray_shasta: task %d node %d >= "
+				      "ntasks %d; skipping",
+				      taskid, nodeidx, ntasks);
+				continue;
+			}
 			pes[taskid].nodeidx = nodeidx;
 			pes[taskid].localidx = localidx;
 
-			// TODO: MPMD support
-			pes[taskid].cmdidx = 0;
+			if (tid_offsets == NULL) {
+				pes[taskid].cmdidx = 0;
+			} else {
+				pes[taskid].cmdidx = tid_offsets[taskid];
+			}
 		}
 	}
 	return pes;
 }
 
 /*
+ * Return an array of pals_cmd_t structures.
+ */
+static pals_cmd_t *_setup_pals_cmds(int ncmds, int ntasks, int nnodes,
+				    int cpus_per_task, uint32_t *tid_offsets)
+{
+	pals_cmd_t *cmds;
+	int i, j;
+
+	cmds = xmalloc(ncmds * sizeof(pals_cmd_t));
+	for (i = 0; i < ncmds; i++) {
+		// For non-MPMD apps, npes = ntasks
+		if (ncmds == 1 || tid_offsets == NULL) {
+			cmds[i].npes = ntasks;
+		} else {
+			// Count number of PEs using the task offsets
+			cmds[i].npes = 0;
+			for (j = 0; j < ntasks; j++) {
+				if (tid_offsets[j] == i) {
+					cmds[i].npes++;
+				}
+			}
+		}
+
+		// We don't have access to per-command values for these,
+		// so set them all to the same value.
+		cmds[i].cpus_per_pe = cpus_per_task;
+		cmds[i].pes_per_node = ntasks / nnodes;
+	}
+
+	return cmds;
+}
+
+/*
  * Fill in the apinfo header
  */
-static void _build_header(pals_header_t *hdr, const stepd_step_rec_t *job)
+static void _build_header(pals_header_t *hdr, int ncmds, int npes, int nnodes)
 {
 	size_t offset = sizeof(pals_header_t);
 
@@ -70,17 +236,17 @@ static void _build_header(pals_header_t *hdr, const stepd_step_rec_t *job)
 
 	hdr->cmd_size = sizeof(pals_cmd_t);
 	hdr->cmd_offset = offset;
-	hdr->ncmds = 1;
+	hdr->ncmds = ncmds;
 	offset += hdr->cmd_size * hdr->ncmds;
 
 	hdr->pe_size = sizeof(pals_pe_t);
 	hdr->pe_offset = offset;
-	hdr->npes = job->ntasks;
+	hdr->npes = npes;
 	offset += hdr->pe_size * hdr->npes;
 
 	hdr->node_size = sizeof(pals_node_t);
 	hdr->node_offset = offset;
-	hdr->nnodes = job->nnodes;
+	hdr->nnodes = nnodes;
 	offset += hdr->node_size * hdr->nnodes;
 
 	hdr->nic_size = sizeof(pals_nic_t);
@@ -150,15 +316,14 @@ static int _writen(int fd, void *buf, size_t len)
 /*
  * Write the job's node list to the file
  */
-static int _write_pals_nodes(const stepd_step_rec_t *job, int fd)
+static int _write_pals_nodes(int fd, char *nodelist)
 {
 	hostlist_t hl;
 	char *host;
 	pals_node_t node;
 
 	memset(&node, 0, sizeof(pals_node_t));
-
-	hl = hostlist_create(job->msg->complete_nodelist);
+	hl = hostlist_create(nodelist);
 	if (hl == NULL) {
 		error("mpi/cray: Couldn't create hostlist");
 		return SLURM_ERROR;
@@ -184,43 +349,87 @@ int create_apinfo(const stepd_step_rec_t *job)
 {
 	int fd = -1;
 	pals_header_t hdr;
-	pals_cmd_t cmd;
+	pals_cmd_t *cmds = NULL;
 	pals_pe_t *pes = NULL;
+	int ntasks, ncmds, nnodes;
+	uint16_t *task_cnts;
+	uint32_t **tids;
+	uint32_t *tid_offsets;
+	char *nodelist;
 
 	// Make sure the application spool directory has been created
 	if (appdir == NULL) {
 		return SLURM_ERROR;
 	}
 
-	// Get information to write
-	_build_header(&hdr, job);
+	// Get relevant information from job
+	if (job->pack_jobid != NO_VAL) {
+		ntasks = job->pack_ntasks;
+		ncmds = job->pack_step_cnt;
+		nnodes = job->pack_nnodes;
+		task_cnts = job->pack_task_cnts;
+		tids = job->pack_tids;
+		tid_offsets = job->pack_tid_offsets;
+		nodelist = job->pack_node_list;
+	} else {
+		ntasks = job->ntasks;
+		nnodes = job->nnodes;
+		task_cnts = job->msg->tasks_to_launch;
+		tids = job->msg->global_task_ids;
+		nodelist = job->msg->complete_nodelist;
 
-	memset(&cmd, 0, sizeof(pals_cmd_t));
-	cmd.npes = job->ntasks;
-	cmd.pes_per_node = job->ntasks / job->nnodes;
-	cmd.cpus_per_pe = job->cpus_per_task;
-
-	pes = _setup_pals_pes(job);
-	if (pes == NULL) {
-		return SLURM_ERROR;
+		if (job->flags & LAUNCH_MULTI_PROG) {
+			_multi_prog_parse(job, &ncmds, &tid_offsets);
+		} else {
+			ncmds = 1;
+			tid_offsets = NULL;
+		}
 	}
+
+	// Make sure we've got everything
+	if (ntasks <= 0) {
+		error("mpi/cray_shasta: no tasks found");
+		goto error;
+	}
+	if (ncmds <= 0) {
+		error("mpi/cray_shasta: no cmds found");
+		goto error;
+	}
+	if (nnodes <= 0) {
+		error("mpi/cray_shasta: no nodes found");
+		goto error;
+	}
+	if (task_cnts == NULL) {
+		error("mpi/cray_shasta: no per-node task counts");
+		goto error;
+	}
+	if (tids == NULL) {
+		error("mpi/cray_shasta: no task IDs found");
+		goto error;
+	}
+	if (nodelist == NULL) {
+		error("mpi/cray_shasta: no nodelist found");
+		goto error;
+	}
+
+	// Get information to write
+	_build_header(&hdr, ncmds, ntasks, nnodes);
+	cmds = _setup_pals_cmds(ncmds, ntasks, nnodes, job->cpus_per_task,
+				tid_offsets);
+	pes = _setup_pals_pes(ntasks, nnodes, task_cnts, tids, tid_offsets);
 
 	// Create the file
 	fd = _open_apinfo(job);
 	if (fd == -1) {
-		xfree(pes);
-		return SLURM_ERROR;
+		goto error;
 	}
 
 	// Write info
 	if (_writen(fd, &hdr, sizeof(pals_header_t)) == SLURM_ERROR ||
-	    _writen(fd, &cmd, sizeof(pals_cmd_t)) == SLURM_ERROR ||
-	    _writen(fd, pes, job->ntasks * sizeof(pals_pe_t)) ==
-		    SLURM_ERROR ||
-	    _write_pals_nodes(job, fd) == SLURM_ERROR) {
-		xfree(pes);
-		close(fd);
-		return SLURM_ERROR;
+	    _writen(fd, cmds, hdr.ncmds * sizeof(pals_cmd_t)) == SLURM_ERROR ||
+	    _writen(fd, pes, hdr.npes * sizeof(pals_pe_t)) == SLURM_ERROR ||
+	    _write_pals_nodes(fd, nodelist) == SLURM_ERROR) {
+		goto error;
 	}
 	// TODO: Write communication profiles
 	// TODO write nics
@@ -228,15 +437,26 @@ int create_apinfo(const stepd_step_rec_t *job)
 	// Flush changes to disk
 	if (fsync(fd) == -1) {
 		error("mpi/cray_shasta: Couldn't sync %s to disk: %m", apinfo);
-		xfree(pes);
-		close(fd);
-		return SLURM_ERROR;
+		goto error;
 	}
 
 	debug("mpi/cray_shasta: Wrote apinfo file %s", apinfo);
 
 	// Clean up and return
+	if (job->flags & LAUNCH_MULTI_PROG) {
+		xfree(tid_offsets);
+	}
 	xfree(pes);
+	xfree(cmds);
 	close(fd);
 	return SLURM_SUCCESS;
+
+error:
+	if (job->flags & LAUNCH_MULTI_PROG) {
+		xfree(tid_offsets);
+	}
+	xfree(pes);
+	xfree(cmds);
+	close(fd);
+	return SLURM_ERROR;
 }
