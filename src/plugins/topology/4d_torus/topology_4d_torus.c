6221f0ac6c8598991384c51e63e44b900f305f3c
Varsha Dubey <30374416+varshadubey244@users.noreply.github.com>
2019-09-18 15:52:50 +0200

    Update topology_4d_torus.c

--- src/plugins/topology/4d_torus/topology_4d_torus.c
+++ src/plugins/topology/4d_torus/topology_4d_torus.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "config.h"
+#include <signal.h>
+#include <sys/types.h>
+
+
 const char plugin_name[]        = "topology 4d_torus plugin";
 const char plugin_type[]        = "topology/4d_torus";
 
