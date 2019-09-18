0c7936b61257df6a07fbfc69e79b69c4960dab3d
Varsha Dubey <30374416+varshadubey244@users.noreply.github.com>
2019-09-18 15:51:22 +0200

    Update topology_4d_torus.c

--- src/plugins/topology/4d_torus/topology_4d_torus.c
+++ src/plugins/topology/4d_torus/topology_4d_torus.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
-main(){
- printf("This is the new plugin");
- }
+const char plugin_name[]        = "topology 4d_torus plugin";
+const char plugin_type[]        = "topology/4d_torus";
+
+extern int init(void)
+{
+	verbose("%s loaded", plugin_name);
+	return SLURM_SUCCESS;
+}
